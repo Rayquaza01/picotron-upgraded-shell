b3509f401ed7fb2c9bffd0c65dd39a559f82c155
Arnaught <rayquaza01@outlook.com>
2024-12-05 14:09:43 -0500

    Add visual feedback to Ctrl+C

--- examples/ctrlc.lua
+++ examples/ctrlc.lua
@@ -2,8 +2,11 @@
 -- Allows you to clear the cmd with Ctrl+C
 -- Install by saving this file to /appdata/system/terminal/ctrlc.lua
 
-local function ctrlc()
+local function ctrlc(v)
 	if key("ctrl") and keyp("c") then
+		if v.cmd ~= "" then
+			add_line(v.get_prompt() .. v.cmd .. "\f8^C\f7")
+		end
 		return { cmd = "" }
 	end
 end
