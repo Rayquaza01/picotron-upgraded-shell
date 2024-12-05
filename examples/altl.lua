33f93e084bb71c641837297f2466aef9b85f3d3d
Arnaught <rayquaza01@outlook.com>
2024-12-05 13:54:00 -0500

    Alt+L list partial path

--- examples/altl.lua
+++ examples/altl.lua
@@ -1,13 +1,16 @@
 --[[pod_format="raw",created="2024-12-05 17:46:19",modified="2024-12-05 18:36:01",revision=40]]
+-- Adds fish style alt+l to list the current directory, or the directory under the cursor
+-- Install by saving this file to /appdata/system/terminal/altl.lua
+
 --- Get the token at the position in the string
 --- @param str string
 --- @param pos integer
---- @returns string
+--- @return string
 local function get_token(str, pos)
 	local tokens = split(str, " ", false)
 
 	local current_position = 1
-	for i, token in ipairs(tokens) do
+	for _, token in ipairs(tokens) do
 		local end_position = current_position + #token
 		if pos >= current_position and pos <= end_position then
 			return token
@@ -16,7 +19,7 @@ local function get_token(str, pos)
 		-- add one to account for spaces
 		current_position = end_position + 1
 	end
-	
+
 	return tokens[#tokens]
 end
 
@@ -25,12 +28,24 @@ local function shortcut(v)
 		if keyp("l") then
 			local token = get_token(v.cmd, v.cursor_pos + 1)
 
+			-- if cursor is under a folder, then list that folder
 			if fstat(token) == "folder" then
 				add_line(v.get_prompt()..v.cmd)
 				v.run_terminal_command("ls " .. token)
 			else
-				add_line(v.get_prompt()..v.cmd)
-				v.run_terminal_command("ls")
+				-- if cursor is under a partially typed path, list up to last /
+				local path_components = split(token, "/", false)
+				deli(path_components)
+				local partial_path = table.concat(path_components, "/")
+
+				if fstat(partial_path) == "folder" then
+					add_line(v.get_prompt()..v.cmd)
+					v.run_terminal_command("ls " .. partial_path)
+				else
+					-- if couldn't find path under cursor, list pwd
+					add_line(v.get_prompt()..v.cmd)
+					v.run_terminal_command("ls")
+				end
 			end
 		end
 	end
